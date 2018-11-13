f213dbbd2aa8c639a2431109175a8cebb033d622
Robin Harms Oredsson <robin@betahaus.net>
2018-11-13 11:47:26 +0100

    Wrong path

--- {{cookiecutter.repo_name}}/install_root.sh
+++ {{cookiecutter.repo_name}}/install_root.sh
@@ -3,7 +3,7 @@
 
 cd /etc/nginx
 ln -s {{cookiecutter.install_dir}}/etc/nginx.conf ./sites-available/{{cookiecutter.instance_name}}.conf
-ln -s ./sites-available/{{cookiecutter.instance_name}}.conf ../sites-enabled/.
+ln -s ./sites-available/{{cookiecutter.instance_name}}.conf ./sites-enabled/.
 
 service nginx stop
 certbot certonly --standalone -d {{cookiecutter.domain_name}}
